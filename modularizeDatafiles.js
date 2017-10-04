ba5b93c3012db4783a3cb57d1475b200885465bd
TobiahRex <tobiah.rex@gmail.com>
2017-10-04 12:07:29 -0700

    push from October 4.

--- modularizeDatafiles.js
+++ modularizeDatafiles.js
@@ -3,5 +3,25 @@ const fs = require('fs');
 fs.readFile('../Trading/rawData/GBPJPY-1M-2002.js', 'utf8', (err, data) => {
   if (err) throw new Error(err);
 
-  console.log('\nData: \n', data.split('GBPJPY,').slice(0, 500));
-})
+  const months = [];
+  let cachedMonth = '01';
+  let iStart = 0;
+  let iFinish = 0;
+
+  data
+  .split('GBPJPY,');
+  .forEach((chunk, i) => {
+    iFinish = i;
+    if (chunk.length) {
+      const date = chunk.split(',')[0];
+      const month = date.slice(4, 6);
+      if (month !== cachedMonth) {
+        cachedMonth = month;
+        months.push({
+          iStart,
+          iFinish,
+        });
+      }
+    }
+  });
+});
